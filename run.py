31c988d3c8055bfbe87842c941c1a2b0932450f3
devmolozi <jeancordeiro.f@gmail.com>
2023-11-23 21:33:35 -0300

    aula 2

--- run.py
+++ run.py
@@ -1,22 +1,51 @@
 from telebot.async_telebot import AsyncTeleBot
 from config import TOKEN
+from telebot import types
 
 bot = AsyncTeleBot(TOKEN)
 
-#RESPONDE O COMANDO /START OU /HELP
-@bot.message_handler(commands=['help', 'start'])
-async def send_welcome(message):
 
-    user_name = message.from_user.username
+@bot.message_handler(commands=['info'])
+async def send_info(message):
+    #user_name = message.from_user.username
+    first_name = message.from_user.first_name
+    language = message.from_user.language_code
+    user_id = message.from_user.id
+
+    texto = f'**BOT - INFO**\n\n'
+    texto += f'Olá {first_name}\n\n'
+    texto += f'🇧🇷 {language}\n\n'
+    texto += f'ID:  {user_id}'
+
+    # VAMOS USAR MARKDOWN PARA FORMATAR NOSSO TEXTO
+    await bot.reply_to(message, texto, parse_mode='Markdown')
+
+
+@bot.message_handler(commands=['ajuda'])
+async def button(message):
     first_name = message.from_user.first_name
 
-    await bot.reply_to(message, f"Seu usuário @{user_name} | Seu nome {first_name}")
+    # CRIAR BOTÕES CLICAVEIS
+    keyboard = types.InlineKeyboardMarkup()
+
+    # BOTÃO PARA ABRIR UMA URL
+    url = types.InlineKeyboardButton('Inscreva-se', url='https://www.google.com/')
+    keyboard.add(url)
+
+    # BOTÃO PARA ENVIAR UMA FUNÇÃO
+    action = types.InlineKeyboardButton('Frase', callback_data='frase_comando')
+    keyboard.add(action)
+
+    await bot.reply_to(message, f'Bem-vindo {first_name}!| Escolha uma opção abaixo',reply_markup=keyboard)
 
 
-#RESPONDE SUA MENSAGEM COM A MESMA MENSAGEM
-@bot.message_handler(func=lambda message: True)
-async def echo_message(message):
-    await bot.reply_to(message, message.text)
+
+@bot.callback_query_handler(func=lambda call: True)
+async def receber_comando(call):
+    # VERIFICA SE A CHAMADA CONTÉM O PARAMETRO PASSADO PELO CALLBACK
+    if call.data == 'frase_comando':
+        # AQUI USAMOS 'message.chat.id' PARA PASSAR COMO UMA MENSAGEM NORMAL...
+        await bot.send_message(call.message.chat.id, 'Um único pensamento pode revolucionar sua vida | Norman Vincent Peale')
 
 
 import asyncio
